8ad81a73e453d09a6cf01ef06488f0a6d3f7962a
Nelson <gmz_nlsn@yahoo.com>
2022-08-10 21:05:29 -0700

    work Completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// const multiply = (num1, num2) => {
+//     return num1 * num2;
+// } = 
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random()) + 1;
+
+
+let num3 = 29;
+let num4 = 5;
+const mod = num3 % num4;
+
+
+const max = Math.max(20);
